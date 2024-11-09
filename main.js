8e91085e912c61702572040d08302a315e5de774
Saikaranam-70 <saikaranam46@gmail.com>
2024-11-09 19:45:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -36,33 +36,6 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 
 
-// let testimonialSwiper = new Swiper(".testimonial-swiper", {
-//     spaceBetween: 30,
-//     loop: 'true',
-
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-// });
-
-
-// let newSwiper = new Swiper(".new-swiper", {
-//     spaceBetween: 24,
-//     loop: 'true',
-
-//     breakpoints: {
-//         576: {
-//           slidesPerView: 2,
-//         },
-//         768: {
-//           slidesPerView: 3,
-//         },
-//         1024: {
-//           slidesPerView: 4,
-//         },
-//     },
-// });
 
 const sections = document.querySelectorAll('section[id]')
     
@@ -94,55 +67,6 @@ const scrollUp = () =>{
 }
 window.addEventListener('scroll', scrollUp)
 
-// const cart = document.getElementById('cart'),
-//       cartShop = document.getElementById('cart-shop'),
-//       cartClose = document.getElementById('cart-close')
-
-
-// if(cartShop){
-//     cartShop.addEventListener('click', () =>{
-//         cart.classList.add('show-cart')
-//     })
-// }
-
-
-// if(cartClose){
-//     cartClose.addEventListener('click', () =>{
-//         cart.classList.remove('show-cart')
-//     })
-// }
-
-
-// const themeButton = document.getElementById('theme-button')
-// const darkTheme = 'dark-theme'
-// const iconTheme = 'bx-sun'
-
-
-// const selectedTheme = localStorage.getItem('selected-theme')
-// const selectedIcon = localStorage.getItem('selected-icon')
-
-
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
-
-
-// if (selectedTheme) {
-
-//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-//   themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
-// }
-
-
-// themeButton.addEventListener('click', () => {
-
-//     document.body.classList.toggle(darkTheme)
-//     themeButton.classList.toggle(iconTheme)
-
-//     localStorage.setItem('selected-theme', getCurrentTheme())
-//     localStorage.setItem('selected-icon', getCurrentIcon())
-// })
-
-// Include ScrollReveal in your project
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
